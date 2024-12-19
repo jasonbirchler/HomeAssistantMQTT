3c52370393b15d1220cf9935d49b5bb7005672fd
Jason Birchler <hugenerd@mac.com>
2024-12-18 20:04:09 -0800

    add conditional statement to handle WiFi for multiple chips

--- src/HomeAssistantMQTT.h
+++ src/HomeAssistantMQTT.h
@@ -1,9 +1,22 @@
 #pragma once
 
 #include "Arduino.h"
-#include "ESP8266WiFi.h"
 #include "PubSubClient.h"
 #include "ArduinoJson.h"
+
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_GIGA) || defined(ARDUINO_OPTA)
+  #include <WiFi.h>
+#elif defined(ARDUINO_PORTENTA_C33)
+  #include <WiFiC3.h>
+#elif defined(ARDUINO_UNOR4_WIFI)
+  #include <WiFiS3.h>
+#endif
 
 #define HAMQTT_MAXITEMS 30
 
